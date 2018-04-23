c4e94e8de56c2dbaf35174f76bd96132bb6ce0cc
Leonardo Bustamante <leobusta@gmail.com>
2018-04-23 11:53:46 -0500

    debug

--- config/logging.php
+++ config/logging.php
@@ -39,9 +39,8 @@ return [
         ],
 
         'single' => [
-            'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
-            'level' => 'debug',
+            'driver' => 'errorlog',
+	    'level' => 'debug',
         ],
 
         'daily' => [
